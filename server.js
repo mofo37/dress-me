9bc3968898315aad816b0dbcad923cad65b9c5c6
Morgan Fogarty <mofo37@gmail.com>
2018-03-28 22:00:01 -0700

    change greeting

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hi everyone!');
 });
 
 server.listen(port, hostname, () => {
